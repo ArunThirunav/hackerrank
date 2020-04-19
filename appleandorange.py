d0ca58fb8f942d1f24a8701a55cdcc5512f9052f
ArunThirunavukkarasu <arunthirunavukkarasu96@gmail.com>
2020-04-19 17:55:33 +0530

    updated the apple and orange problem with exact answer

--- appleandorange.py
+++ appleandorange.py
@@ -4,7 +4,6 @@ import random
 import re
 import sys
 from array import *
-from itertools import cycle
 
 def countApplesAndOranges(s, t, a, b, apples, oranges):
     c1 = 0
@@ -17,11 +16,9 @@ def countApplesAndOranges(s, t, a, b, apples, oranges):
         orangenewpos = int(j) + b
         if s <= orangenewpos <= t:
             c2 +=1
-    
-    print(applesNew, orangesNew)
-    print(c1, c2)
-    print(type(applesNew))
-    return (c1, c2)  
+
+    print(c1)
+    print(c2) 
 
 
 if __name__ == '__main__':    
@@ -35,20 +32,21 @@ if __name__ == '__main__':
     m = int(mn[0])
     n = int(mn[1])
     apples = list(map(int, input().rstrip().split()))
-    apples = array('i', apples)
+    apples = array('q', apples)
     oranges = list(map(int, input().rstrip().split()))
-    oranges = array('i', oranges)
-    print(apples)
-    print(type(apples))
-    print(oranges)
-    print(type(oranges))
+    oranges = array('q', oranges)
 
     countApplesAndOranges(s, t, a, b, apples, oranges)
 
 """
+Test Case: 
 7 11
 5 15
 3 2
 -2 2 1
 5 -6
+
+Expected Answer:
+1
+1
 """
